3e9021aad1a9da51d6cfb7ece4f44d2beaea15ab
SOUMALYA DAS <144205967+soumalya19@users.noreply.github.com>
2025-04-09 19:12:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 EMAIL = "imap.gmail.com"   # IMAP server for Gmail
-EMAIL = "sohandas1906@gmail.com"
-PASSWORD = "tukg ndnn ggwt fckt" # App password, not real password
+EMAIL = "                      " #email id
+PASSWORD = "                   " # App password, not real password
 
